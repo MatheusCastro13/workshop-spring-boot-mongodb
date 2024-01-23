5b1357abfe09554d5b8b096e639fd5ae23b67339
MatheusCastro13 <matheuscastro1304@gmail.com>
2024-01-23 16:05:46 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/matheusResio/workshopmongo/config/Instatiation.java
@@ -0,0 +1,30 @@
+package com.matheusResio.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.matheusResio.workshopmongo.domain.User;
+import com.matheusResio.workshopmongo.repositories.UserRepository;
+
+@Configuration
+public class Instatiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
