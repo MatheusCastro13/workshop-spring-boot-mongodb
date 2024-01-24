d3f827800829fc9b14e4f7132922d7c8b695de05
MatheusCastro13 <matheuscastro1304@gmail.com>
2024-01-24 08:39:54 -0300

    Insercao de usuario com POST

--- src/main/java/com/matheusResio/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusResio/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheusResio.workshopmongo.domain.User;
+import com.matheusResio.workshopmongo.dto.UserDTO;
 import com.matheusResio.workshopmongo.repositories.UserRepository;
 import com.matheusResio.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return user.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
